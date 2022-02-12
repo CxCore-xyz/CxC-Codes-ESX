63c56e23e472af2c30fbfaab9c372f079e7108ca
CxCore.xyz <91425802+CxCore-xyz@users.noreply.github.com>
2022-02-12 16:05:59 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.CuxAllow = "god" -- Permissions to create a code.
+Config.CuxL = 10 -- Code Length.
+
+Config.CuxHook = "https://discord.com/api/webhooks/924648075048390656/ic5MwgRkiJJR2cPMFjUxA6aYeff4EJ-n22_Vn2DjUt4GnbQknQ74WpVCG7yN4sYPJLLO" -- Discord Logs WeebHook.
